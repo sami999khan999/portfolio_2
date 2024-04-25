b6860e3797143035b0ca4a864ef1fb8fec4b0a0d
Sami Khan <sami999khan999@gmail.com>
2024-04-25 23:41:42 +0600

    done

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -20,7 +20,7 @@ const NavLinks = ({
     <div className={className}>
       <ul className={`flex items-center gap-8 ${ulClass}`}>
         {navLinks.map((link, index) => (
-          <li>
+          <li key={index}>
             <Link
               key={index}
               className={`py-3 text-gray-700 hover:text-purple transition-colors relative block after:block after:absolute after:left-0 after:h-0.5 after:bg-purple after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-right after:hover:origin-left ${className} ${navLinkClass} ${
